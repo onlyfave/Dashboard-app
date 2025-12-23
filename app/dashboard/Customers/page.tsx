8b4c3b42edae2d20b68c302e528743e39f535810
Fave <onyenekeflavour@gmail.com>
2025-12-23 19:46:34 +0100

    initial commit

---
+++ app/dashboard/Customers/page.tsx
@@ -0,0 +1,5 @@
+<p>Customers Page</p>
+
+export default function Page() {
+  return <p>Customers Page</p>;
+}
---
+++ app/dashboard/Invoices/page.tsx
@@ -0,0 +1,5 @@
+<p>Invoices Page</p>
+
+export default function Page() {
+  return <p>Invoices Page</p>;
+}
